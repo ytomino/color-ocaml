a5e1f0d58929d6c71af0e12ed297ced814a99a9a
ytomino <demoonlit@panathenaia.halfmoon.jp>
2023-03-29 15:40:38 +0900

    Add the examples

---
+++ examples/test_conv.ml
@@ -0,0 +1,25 @@
+let diff = 0x0001p-16;;
+
+for ired = 0 to 4 do
+	for igreen = 0 to 4 do
+		for iblue = 0 to 4 do
+			let rgb = Color.RGB.make ~red:(float_of_int ired /. 4.0)
+				~green:(float_of_int igreen /. 4.0) ~blue:(float_of_int iblue /. 4.0)
+			in
+			(* sRGB *)
+			let srgb = Color.srgb_of_rgb rgb in
+			let via_srgb = Color.rgb_of_srgb srgb in
+			assert (Color.RGB.distance via_srgb rgb < diff);
+			(* HSV *)
+			let hsv = Color.hsv_of_rgb rgb in
+			let via_hsv = Color.rgb_of_hsv hsv in
+			assert (Color.RGB.distance via_hsv rgb < diff);
+			(* HSL *)
+			let hsl = Color.hsl_of_rgb rgb in
+			let via_hsl = Color.rgb_of_hsl hsl in
+			assert (Color.RGB.distance via_hsl rgb < diff)
+		done
+	done
+done;;
+
+Printf.eprintf "ok\n";;
